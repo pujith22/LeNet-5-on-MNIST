3ac1bf25e278e76c574c473e5d148d4b25d96a61
pujith22 <pujith22.sde@gmail.com>
2025-11-24 22:23:18 +0530

    first blood! the project template setup.

---
+++ src/models/lenet.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class LeNet5(nn.Module):
+    """
+    LeNet-5 architecture as described in Lecun et al., 1998.
+    """
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        # TODO: Implement layers
+        pass
+
+    def forward(self, x):
+        # TODO: Implement forward pass
+        return x
---
+++ src/train.py
@@ -0,0 +1,9 @@
+import torch
+from src.models.lenet import LeNet5
+
+def train():
+    # TODO: Implement training loop
+    print("Training not implemented yet.")
+
+if __name__ == "__main__":
+    train()
